ed91f7420a37639a1de267f9ca26441c60bcfff8
huangxiaoxue <229396865@qq.com>
2017-10-19 22:38:54 +0800

    taskInstance page 作业实例页面

--- log_record/views.py
+++ log_record/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.http import HttpResponse
 from job.models import *
+from job.job_management import (Task)
 from controller.core.public import *
 import json
 # Create your views here.
@@ -12,13 +13,13 @@ import json
 
 @login_required
 def taskInstanceList(request):
-    # 首页
-    return render_to_response('log_record/taskInstansList.html', locals(), context_instance=RequestContext(request))
+    # 作业实例列表首页
+    return render_to_response('log_record/taskInstanceList.html', locals(), context_instance=RequestContext(request))
 
 
 @login_required
-def get_taskInstance(request):
-    # 获取任务实例列表
+def get_taskInstance_list(request):
+    # 获取所有作业实例
     data = Nm_Instance.objects.all().values()
     data_str = [transfor(d) for d in data]
     response = HttpResponse()
@@ -40,6 +41,28 @@ def transfor(d):
 
 
 @login_required
-def taskInstance(request, id):
+def task_instance(request, id):
     # 作业详情
-    return render_to_response('log_record/taskStepInstance.html', locals(), context_instance=RequestContext(request))
+    return render_to_response('log_record/taskInstance.html', locals(), context_instance=RequestContext(request))
+
+
+@login_required
+def get_task_instance(request):
+    # 获取作业实例
+    cur = Currency(request)
+    _id = cur.rq_post('taskInstanceId')
+    print _id
+    dtf = DataTransfer()
+    instance = Nm_Instance.objects.values().get(pk=int(_id))
+    nm_stepset = Nm_StepInstance.objects.filter(taskInstanceId=instance['id']).values()
+    nm_step = [dtf.commonTransfor1(d) for d in nm_stepset]
+    nm_step.sort(cmp=Task.cmp, reverse=True)
+    data = {'nm_task': dtf.commonTransfor1(instance), 'nm_step': nm_step}
+    import pprint
+    pprint.pprint(data)
+    # print data
+    # data = Nm_Instance.objects.all().values()
+    # data_str = [transfor(d) for d in data]
+    response = HttpResponse()
+    response.write(json.dumps(data))
+    return response
